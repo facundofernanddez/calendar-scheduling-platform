f16617af50aca5dd9f0ee13ca3c85b8592708663
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2025-07-15 10:19:56 -0300

    create navbar and traying to put a logo

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,3 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+
 export default function Navbar() {
-  return <div>navbar</div>;
+  return (
+    <div className="flex py-5 items-center justify-between">
+      <Link
+        href={"/"}
+        className="flex items-center gap-2"
+      >
+        <Image
+          src={CalendarMonthIcon}
+          alt="Logo"
+          className="size-10"
+        ></Image>
+        <h4 className="text-3xl font-semibold">
+          Facundo <span className="text-blue-500">Fernandez</span>
+        </h4>
+      </Link>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Navbar from "./components/Navbar";
+
 export default function Home() {
-  return <div>Hola</div>;
+  return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <Navbar />
+    </div>
+  );
 }
